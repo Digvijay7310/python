4a5b69298345662bd0ee6b0ec70f2d1f57ca46ed
Digvijay7310 <kumardigvijay377@gmail.com>
2025-11-25 13:34:49 +0530

    learn while loop with practice questions.

---
+++ while.py
@@ -0,0 +1,106 @@
+#while loop
+
+# Print numbers form 1 to 100
+# i=1
+# while i <= 100 :
+#   print(i)
+#   i += 1
+
+# print("Loop End")
+
+
+# Print numbers form 100 to 1
+# count = 100
+# while count >= 1:
+#     print(count)
+#     count -= 1
+
+# print("Loop End")
+
+
+
+# Print the multiplication table of a number n
+# num = int(input("Enter the number: "))
+
+# i = 1
+# while i <= 10:
+#     print(num, "*", i, "=" , i*num)
+#     i += 1
+
+# print("Table end")
+
+# Print the element of the following list
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 82, 100]
+# heros = ["ironman", 'thor', 'superman', 'batman']
+
+# idx = 0
+
+# while idx < len(nums): 
+#     print(nums[idx])
+#     idx += 1
+
+# hero = 0
+# while hero < len(heros):
+#     print(heros[hero])
+
+#     hero += 1
+
+
+# /search for a number x in this tuple using loop
+# nums = (1, 4, 9, 16, 25, 36, 49, 64, 82, 100)
+
+# x = 36
+# i = 0
+
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print('Found at index', i, "element", nums[i])
+#         break
+#     else: 
+#         print("finding...")
+#     i += 1
+
+
+
+# break and continue
+
+# i = 1
+# while i < 5:
+#     print(i)
+#     if(i == 3):
+#         break
+#     i += 1
+
+# print("End of the loop")
+
+# find x and print previous and next not x
+# i = 0
+# while i <= 5:
+#     if(i == 3):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+# Print odd number 
+# i = 1
+
+# while i <= 10:
+#     if(i % 2 != 0):
+#         print(i)
+#     i += 1
+
+
+# Print even number of the between two numbers
+
+# i = int(input("Enter the first number: "))
+# j = int(input("Enter second number: "))
+
+# while j:
+#     if(i % 2 == 0):
+#         print(i)
+#     if(i == j): 
+#         break
+#     i += 1
+# print("Loop end")    
+    
