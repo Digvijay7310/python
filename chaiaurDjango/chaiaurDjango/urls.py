1e27eeb12b99b7d5d59617fbd5fd61a5436fc8d7
Digvijay7310 <kumardigvijay377@gmail.com>
2025-12-01 16:26:00 +0530

    add tailwind, settings for migration with admin username and password

--- chaiaurDjango/chaiaurDjango/urls.py
+++ chaiaurDjango/chaiaurDjango/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('chai/', include('chai.urls') ),
+
+    path("_reload_/", include("django_browser_reload.urls")),
 ]
